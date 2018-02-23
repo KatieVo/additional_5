5b71998f4314bd91eebe7532435228e5e9eb7329
Kate <ekaterinavolkovaalex@gmail.com>
2018-02-23 14:30:50 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length%2) {
+    return false;
+  }
+
+  var map = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    map[bracketsConfig[i][0]] = bracketsConfig[i][1]; 
+  }
+  
+  str = str.split(''); 
+  
+  var brackets = [];
+  for (let i = 0; i < str.length; i++) {
+    if (map[str[i]]) {
+      brackets.push(str[i]); 
+        
+    } else {
+      var lastPushed = brackets.pop();
+      if (map[lastPushed] !== str[i]) {
+        return false;
+      }
+    } 
+
+    if (map[str[i]] === str[i] && brackets[brackets.length-2] === str[i]) {
+      brackets.pop();
+      brackets.pop();
+    }
+  }
+  
+  return brackets.length ? false : true;
+};
